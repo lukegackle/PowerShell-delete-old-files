2e08696637e39acba4544eb62511c3136ed7787f
Luke Gackle <33621585+lukegackle@users.noreply.github.com>
2020-09-27 16:21:05 +0930

    Add files via upload

---
+++ Delete old downloads 14 days old and specific file extensions.ps1
@@ -0,0 +1,16 @@
+﻿$comparison = (get-date).adddays(-14)
+
+Get-ChildItem "C:\Users\$([Environment]::UserName)\Downloads" -Recurse | foreach($_){
+if([datetime]($_.LastWriteTime) -lt $comparison){
+    Remove-Item ($_.FullName)
+}
+else {
+    #For particular file types, remove them regardless of age
+    if(($_.Extension) -contains ".pdf" -or ($_.Extension) -contains ".jpg" -or ($_.Extension) -contains ".dat"){
+        Remove-Item ($_.FullName)
+    }
+}
+}
+
+
+
